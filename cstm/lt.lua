ed9e0403e30ae4ebbc411f213ae3c9932f8d7034
Muhammad Zidan Zibril Irzami <93841944+zdanx@users.noreply.github.com>
2026-08-04 07:36:56 +0700

    Update lt.lua

--- cstm/lt.lua
+++ cstm/lt.lua
@@ -14,6 +14,7 @@ end
 local g = getgenv()
 local Raw_Version = "V9.2.3"
 getgenv().Script_Version = tostring(Raw_Version).."-LifeHub"
+getgenv().Script_Creator = "Zami"
 local Players = g.Players or cloneref and cloneref(game:GetService("Players")) or game:GetService("Players")
 local localPlayer = Players.LocalPlayer or Players:GetPropertyChangedSignal("LocalPlayer"):Wait()
 g.Keybind_Input_Disabled_For_Mini_Game = g.Keybind_Input_Disabled_For_Mini_Game or false
