d4041de2c744cbb03209b00cb2eff1f32e57970c
Muhammad Zidan Zibril Irzami <93841944+zdanx@users.noreply.github.com>
2026-08-25 10:45:48 +0700

    Update loading frame version text

--- cstm/source.lua
+++ cstm/source.lua
@@ -1747,7 +1747,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 	LoadingFrame.Subtitle.Text = Settings.LoadingSubtitle or "Interface Suite"
 
 	if Settings.LoadingTitle ~= "Rayfield Interface Suite" then
-		LoadingFrame.Version.Text = "Zami UI"
+		LoadingFrame.Version.Text = "Rayfield UI x Zami"
 	end
 
 	if Settings.Icon and Settings.Icon ~= 0 and Topbar:FindFirstChild('Icon') then
