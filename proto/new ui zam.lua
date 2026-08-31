ebcc09f079bf01e80b23eb659197c041de20f139
Muhammad Zidan Zibril Irzami <93841944+zdanx@users.noreply.github.com>
2026-09-01 00:00:06 +0700

    Update new ui zam.lua

--- proto/new ui zam.lua
+++ proto/new ui zam.lua
@@ -18,13 +18,13 @@ local ConfigManager = Compkiller:ConfigManager({
 });
 
 -- Loading UI (Icon <string> , Duration <number>) --
-Compkiller:Loader("rbxassetid://94364486491269" , 2.5).yield();
+Compkiller:Loader("rbxassetid://75680508301013" , 2.5).yield();
 
 -- Creating Window --
 local Window = Compkiller.new({
 	Name = "COMPKILLER",
 	Keybind = "LeftAlt",
-	Logo = "rbxassetid://94364486491269",
+	Logo = "rbxassetid://75680508301013",
 	--Scale = Compkiller.Scale.Window,  Leave blank if you want automatic scale [PC, Mobile].
 	TextSize = 15,
 });
@@ -35,7 +35,7 @@ Notifier.new({
 	Title = "Notification",
 	Content = "Thank you for use this script!",
 	Duration = 10,
-	Icon = "rbxassetid://94364486491269"
+	Icon = "rbxassetid://75680508301013"
 });
 
 -- Watermark --
